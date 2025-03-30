0984f18beb72bf55d8b074f0c924f0e83acba04b
Irán Pérez <iran_phantom@hotmail.com>
2025-03-30 15:07:14 -0600

    CSS con Tailwind

--- front/src/components/Card.tsx
+++ front/src/components/Card.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const Card: React.FC<IProduct> = ({name, price, description, image}) => {
     return (
         <div className='w-full max-w-sm bg-white border border-gray-400 rounded-lg min-h-[300px] justify-around items-center gap-1'>
-            <h3>{name}</h3>
+            <h3 className='font-extrabold text-2xl p-3 flex justify-center'>{name}</h3>
             <img className='p-8 rounded-t-lg' src={image} alt={name} />
-            <p>Description: {description}</p>
-            <p>Price: ${price} USD</p>
+            <p className='p-4 truncate'>Description: {description}</p>
+            <p className='p-4 font-semibold bg-gray-700 text-white'>Price: ${price} USD</p>
         </div>
     )
 }
